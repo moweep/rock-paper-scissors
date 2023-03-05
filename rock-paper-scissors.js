ae9fd2256d91b24d288a23e17460be8c9d071d37
Jessica <indecorum72@gmail.com>
2023-03-04 17:50:22 -0800

    Update code which prints to the console the computer's choice of either rock, paper, or scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,12 +2,12 @@ function getComputerChoice(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-	const num = getComputerChoice(1, 3)
-	
-	if (num == 1) {
-		console.log("Computer picked scissors!");
-	} else if (num == 2) {
-		console.log("Computer picked rock!");
-	} else if (num == 3) {
-		console.log("Computer picked paper!");
-	}
+num = getComputerChoice(1, 3);
+
+if (num === 1) {
+	console.log("Rock");
+} else if (num === 2) {
+	console.log("Paper");
+} else if (num === 3) {
+	console.log("Scissors");
+}
